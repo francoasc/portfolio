e720d25eb8bab15ec1ad6fcb3700e954cea615c4
francoasc <francoascarate349@gmail.com>
2021-04-22 19:26:30 -0300

    Navbar prototype

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { debounce } from "../../utilities/debounce";
+
+const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = debounce(() => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  }, 100);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
+  const navbarStyles = {
+    position: "fixed",
+    height: "60px",
+    width: "100%",
+    backgroundColor: "grey",
+    textAlign: "center",
+    transition: "top 0.6s",
+  };
+
+  return (
+    <div style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}>Test</div>
+  );
+};
+
+export default Navbar;
