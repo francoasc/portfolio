018f83f2ceb451095beb1c555187e2d4ba305072
francoasc <francoascarate349@gmail.com>
2021-04-22 19:26:49 -0300

    navbar added to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Home from "./components/Home/home";
+import Navbar from "./components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar /> */}
       <Home />
     </div>
   );
