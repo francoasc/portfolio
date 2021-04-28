2378ace27085d4d9426a82364e92ec53cfb7abc8
francoasc <francoascarate349@gmail.com>
2021-04-27 22:25:28 -0300

    added about to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import About from "./components/About/about";
 import Home from "./components/Home/home";
-import Navbar from "./components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
       <Home />
+      <About />
     </div>
   );
 }
