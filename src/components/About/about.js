5178fe485ab51bc5bc011cab03d1d6646828c0fc
francoasc <francoascarate349@gmail.com>
2021-04-28 23:52:38 -0300

    changes to about screen

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { StyledAbout } from "../../styles/about-styles";
+import { StyledAbout, StyledSkillIcon } from "../../styles/about-styles";
 import coding_image from "../../images/coding_normal.svg";
+import { AiOutlineLinkedin, AiFillGithub } from "react-icons/ai";
+import { skills } from "../../utilities/icons";
 
 function About() {
+  const openLinkHandler = (url) => window.open(url);
+
   return (
     <StyledAbout>
       <div className="container">
         <div className="about_me">
-          <h1 className="title">More about me!</h1>
+          <h4 className="title">More about me</h4>
           <p className="description">
             I'm a full stack web developer and mobile developer currently based
             on Argentina. Since child my curiosity for the internet woke and I
@@ -17,14 +21,52 @@ function About() {
             I love programming and see how from nowhere you can build
             fascinating things like this web for example 😉
           </p>
+          <hr />
+
+          <div className="skills_container">
+            <h4>SKILLS</h4>
+            <p>
+              Take a look at my <span>CV</span> 📃
+            </p>
+            <div className="skills">
+              {skills?.map((skill) => {
+                if (skill.test) {
+                  console.log(skill.test());
+                }
+                return (
+                  <StyledSkillIcon key={skill.name}>
+                    {skill.icon}
+                    <div className="skill_name">{skill.name}</div>
+                  </StyledSkillIcon>
+                );
+              })}
+            </div>
+          </div>
         </div>
         <div className="social_links">
-          <img
-            style={{ width: "523px", height: "340px" }}
-            id="social_img"
-            alt="coding_image"
-            src={coding_image}
-          />
+          <div>
+            <img id="social_img" alt="coding_image" src={coding_image} />
+          </div>
+          <h3>You can find me in:</h3>
+          <p
+            id="github-p"
+            onClick={() => openLinkHandler("https://github.com/francoasc")}
+          >
+            <span>
+              <AiFillGithub size="1.3rem" />
+              GitHub: @francoasc
+            </span>
+          </p>
+          <p
+            onClick={() =>
+              openLinkHandler("https://www.linkedin.com/in/fhascarate/")
+            }
+          >
+            <span>
+              <AiOutlineLinkedin size="1.3rem" />
+              LinkedIn: @fhascarate
+            </span>
+          </p>
         </div>
       </div>
     </StyledAbout>
--- src/styles/about-styles.js
+++ src/styles/about-styles.js
@@ -22,6 +22,7 @@ export const StyledAbout = styled.section`
   .container {
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+
     grid-template-areas: "about-me social-links";
     padding-bottom: 50px;
     padding-top: 50px;
@@ -31,34 +32,114 @@ export const StyledAbout = styled.section`
 
     .about_me {
       grid-area: about-me;
-      border: 2px solid red;
-      margin-left: 0.3rem;
-      padding-left: 5rem;
-      display: flex;
-      flex-wrap: wrap;
-
+      /* border: 2px solid red; */
+      width: 100%;
+      margin: 0 1em;
       .title {
-        align-self: center;
-        font-size: 26px;
+        font-size: 1.7rem;
+        margin-top: 1.5rem;
+        margin-bottom: 1.5rem;
       }
       .description {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: fit-content;
+        font-size: 1rem;
+        margin-bottom: 1rem;
+        word-wrap: break-word;
       }
       .description_2 {
-        height: fit-content;
+        font-size: 1rem;
+        padding-bottom: 1rem;
+        border-bottom: 1px solid gray;
+      }
+
+      .skills_container {
+        display: flex;
+        flex-direction: column;
+        h4 {
+          font-size: 1.7rem;
+          margin-top: 2rem;
+        }
+        p {
+          margin-top: 1rem;
+          span {
+            cursor: pointer;
+            text-decoration: underline;
+            :hover {
+            }
+          }
+        }
+        .skills {
+          width: 80%;
+          display: flex;
+          flex-wrap: wrap;
+        }
       }
     }
 
     .social_links {
       grid-area: social-links;
       margin-right: 0.3rem;
-      border: 2px solid green;
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: column;
+
+      div {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+
+        #social_img {
+          margin-top: 2.8rem;
+          max-width: 100%;
+          max-height: 340px;
+        }
+      }
+
+      h3 {
+        margin-top: 1.5rem;
+        align-self: center;
+        font-size: 1.7rem;
+      }
+
+      #github-p {
+        margin-bottom: 0.25rem;
+        margin-top: 0.5rem;
+      }
+
+      p {
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        text-decoration: none;
+        color: #17a2b8;
+        /* color: #002d62; */
+
+        .icon {
+          /* width: 1.2rem;
+          height: 1.2rem; */
+        }
+
+        span {
+          margin-left: 0.3rem;
+          font-weight: 800;
+        }
+        &:hover {
+          cursor: pointer;
+          text-decoration-line: underline;
+        }
+      }
     }
-    #social_img {
-      width: 100%;
+  }
+`;
+
+export const StyledSkillIcon = styled.span`
+  transition: 0s;
+  margin: 1.5rem;
+  text-align: center;
+  .icons {
+    :hover {
+      transition-duration: 0s;
+      transition-delay: 0s;
+      fill: ${({ children }) => children[0].props.theme};
     }
   }
 `;
