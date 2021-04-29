2e586d04dbfe578e3b5727e5cc1e2bbc4de2706b
francoasc <francoascarate349@gmail.com>
2021-04-29 20:16:43 -0300

    skill icons size changed

--- src/utilities/icons.js
+++ src/utilities/icons.js
@@ -4,31 +4,31 @@ import { SiRedux, SiPostgresql, SiGraphql, SiMongodb } from "react-icons/si";
 
 export const skills = [
   {
-    icon: <IoLogoJavascript size="3rem" theme="#F7DF1E" className="icons" />,
+    icon: <IoLogoJavascript size="5rem" theme="#F7DF1E" className="icons" />,
     name: "JavaScript",
   },
   {
-    icon: <FaNodeJs size="3rem" theme="#88C043" className="icons" />,
+    icon: <FaNodeJs size="5rem" theme="#88C043" className="icons" />,
     name: "NodeJS",
   },
   {
-    icon: <FaReact size="3rem" theme="#7BDEFD" className="icons" />,
+    icon: <FaReact size="5rem" theme="#7BDEFD" className="icons" />,
     name: "ReactJS",
   },
   {
-    icon: <SiRedux size="3rem" theme="#764ABC" className="icons" />,
+    icon: <SiRedux size="5rem" theme="#764ABC" className="icons" />,
     name: "Redux",
   },
   {
-    icon: <SiGraphql size="3rem" theme="#E10098" className="icons" />,
+    icon: <SiGraphql size="5rem" theme="#E10098" className="icons" />,
     name: "GraphQL",
   },
   {
-    icon: <SiPostgresql size="3rem" theme="#336791" className="icons" />,
+    icon: <SiPostgresql size="5rem" theme="#336791" className="icons" />,
     name: "PostgreSQL",
   },
   {
-    icon: <SiMongodb size="3rem" theme="#34A542" className="icons" />,
+    icon: <SiMongodb size="5rem" theme="#34A542" className="icons" />,
     name: "MongoDB",
   },
 ];
