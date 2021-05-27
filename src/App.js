4f0c3ea430714c0bcec60fb6a9202036fae81bec
francoasc <francoascarate349@gmail.com>
2021-05-27 17:04:14 -0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,18 @@ import React, { useEffect } from "react";
 import About from "./components/About";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
-// import Navbar from "./components/Navbar/navbar";
 import { setTheme } from "./utilities/theme";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import Contact from "./components/Contact";
 
 function App() {
   useEffect(() => {
-    AOS.init({ duration: 1000 });
-
-    if (!localStorage.getItem("theme")) {
-      setTheme("light");
-    } else {
-      setTheme(localStorage.getItem("theme"));
-    }
+    if (!localStorage.getItem("theme")) setTheme("light");
+    else setTheme(localStorage.getItem("theme"));
   }, []);
 
   return (
     <div className="App">
       <Home />
-      {/* <Navbar /> */}
       <About />
       <Projects />
       <Contact />
